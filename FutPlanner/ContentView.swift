8ef628cdd3fed097ef052c8ee7c3b5e4171db80c
papiricoh <slendeman0200@gmail.com>
2024-01-15 12:45:57 +0100

    added loading

--- FutPlanner/ContentView.swift
+++ FutPlanner/ContentView.swift
@@ -10,52 +10,44 @@ import SwiftData
 
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
     @State private var selection = 2
     @State public var logged = false
+    @State public var loading = false
     
     var body: some View {
-        if(!logged) {
-            LogInView(onLoginSuccess: {
-                self.logged = true
-            });
-        }else {
-            NavigationView() {
-                TabView(selection:$selection) {
-                    Text("Stadistics").tabItem {
-                        Image(systemName: "chart.line.uptrend.xyaxis")
-                        Text("Evaluaciones")
-                    }.tag(1)
-                    HomeTab().tabItem {
-                        Image(systemName: "soccerball")
-                        Text("Principal")
-                    }.tag(2)
-                    Text("Team").tabItem {
-                        Image(systemName: "sportscourt")
-                        Text("Equipo")
-                    }.tag(3)
+        ZStack() {
+            if(!logged) {
+                LogInView(onLoginSuccess: {
+                    self.logged = true
+                });
+            }else {
+                NavigationView() {
+                    TabView(selection:$selection) {
+                        Text("Stadistics").tabItem {
+                            Image(systemName: "chart.line.uptrend.xyaxis")
+                            Text("Evaluaciones")
+                        }.tag(1)
+                        HomeTab().tabItem {
+                            Image(systemName: "soccerball")
+                            Text("Principal")
+                        }.tag(2)
+                        Text("Team").tabItem {
+                            Image(systemName: "sportscourt")
+                            Text("Equipo")
+                        }.tag(3)
+                    }
                 }
             }
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
+            if(loading) {
+                Text("CARDANDO...")
             }
         }
+        
     }
+
+    
 }
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
 }
