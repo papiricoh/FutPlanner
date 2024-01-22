908a8ab699fccbe24afc8449ad5eb679c41a9f83
papiricoh <slendeman0200@gmail.com>
2024-01-22 13:16:59 +0100

    Added format to placesList

---
+++ FutPlanner/Components/ListItem.swift
@@ -0,0 +1,18 @@
+//
+//  ListItem.swift
+//  FutPlanner
+//
+//  Created by Pablo Rico alvarez on 22/1/24.
+//
+
+import SwiftUI
+
+struct ListItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListItem()
+}
