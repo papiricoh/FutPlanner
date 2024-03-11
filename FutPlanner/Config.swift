ee744db266ff8018af4c03aa46f625da499b5f2d
papiricoh <slendeman0200@gmail.com>
2024-03-11 02:22:54 +0100

    added firsts steps on fetch

---
+++ FutPlanner/Config.swift
@@ -0,0 +1,10 @@
+//
+//  Config.swift
+//  FutPlanner
+//
+//  Created by Pablo Rico alvarez on 11/3/24.
+//
+
+import Foundation
+
+var apiDir: String = "46.4.74.141:8080"
--- FutPlanner/Objects/Utils/ModelLoader.swift
+++ FutPlanner/Objects/Utils/ModelLoader.swift
@@ -37,3 +37,23 @@ func load<T: Decodable>(_ filename: String) -> T {
         fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
     }
 }
+/*
+extension URLSession {
+  func fetchData(at url: URL, completion: @escaping (Result<[ToDo], Error>) -> Void) {
+    self.dataTask(with: url) { (data, response, error) in
+      if let error = error {
+        completion(.failure(error))
+      }
+
+      if let data = data {
+        do {
+          let toDos = try JSONDecoder().decode([ToDo].self, from: data)
+          completion(.success(toDos))
+        } catch let decoderError {
+          completion(.failure(decoderError))
+        }
+      }
+    }.resume()
+  }
+}
+*/
